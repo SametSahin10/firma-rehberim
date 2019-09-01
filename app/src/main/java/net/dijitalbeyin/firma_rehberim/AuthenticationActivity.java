7adc786ea16ddbb5ff8eb4c85efe55e86eb0f436
Samet Sahin <a.sahin.ual@gmail.com>
2019-09-01 17:36:51 +0300

    Switched to a NoActionBar theme on AuthenticationActivity, minor UI changes

--- app/src/main/java/net/dijitalbeyin/firma_rehberim/AuthenticationActivity.java
+++ app/src/main/java/net/dijitalbeyin/firma_rehberim/AuthenticationActivity.java
@@ -1,13 +1,30 @@
 package net.dijitalbeyin.firma_rehberim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AuthenticationActivity extends AppCompatActivity {
+
+    EditText et_username;
+    EditText et_password;
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AuthenticationActivity.this, RadiosActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
