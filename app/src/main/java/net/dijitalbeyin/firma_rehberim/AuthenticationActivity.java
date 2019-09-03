89af85df4faf97fa832828da38b6b0ba8cfa82c0
Samet Sahin <a.sahin.ual@gmail.com>
2019-09-03 15:15:32 +0300

    Added an option to skip logging in on AuthenticationActivity

--- app/src/main/java/net/dijitalbeyin/firma_rehberim/AuthenticationActivity.java
+++ app/src/main/java/net/dijitalbeyin/firma_rehberim/AuthenticationActivity.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
     EditText et_username;
     EditText et_password;
     Button btn_login;
+    TextView et_continue_without_logging_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,14 @@ public class AuthenticationActivity extends AppCompatActivity {
 
         btn_login = findViewById(R.id.btn_login);
         btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Implement authtentication logic.
+            }
+        });
+
+        et_continue_without_logging_in = findViewById(R.id.tv_continue_without_logging_in);
+        et_continue_without_logging_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(AuthenticationActivity.this, RadiosActivity.class);
