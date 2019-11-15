9162e6f2de16159d4e508f1609ebaf312f693de2
SametSahin10 <a.sahin.ual@gmail.com>
2019-11-16 01:26:19 +0300

    Renamed package name

---
+++ app/src/main/java/com/firmarehberim/radyo/datamodel/PlaybackStatus.java
@@ -0,0 +1,6 @@
+package com.firmarehberim.radyo.datamodel;
+
+public enum PlaybackStatus {
+    PLAYING,
+    PAUSED
+}
---
+++ app/src/main/java/com/firmarehberim/radyo/fragments/TvFragment.java
@@ -0,0 +1,22 @@
+package com.firmarehberim.radyo.fragments;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firmarehberim.radyo.R;
+
+public class TvFragment extends Fragment {
+    @Nullable
+    public View onCreateView(@NonNull LayoutInflater layoutInflater, @Nullable ViewGroup viewGroup, @Nullable Bundle bundle) {
+        return layoutInflater.inflate(R.layout.fragment_tv, viewGroup, false);
+    }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle bundle) {
+        super.onViewCreated(view, bundle);
+    }
+}
